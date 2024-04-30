3bc29beaac4596f37731766dc9e3eb6ede7d56bf
Tommy0901 <ak710180@gmail.com>
2024-04-30 17:54:20 +0800

    [HTML, CSS] Add title and filters

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,14 @@ import { join } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const port = 3000;
 
-  app.setBaseViewsDir(join(__dirname, '../views'));
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('pug');
 
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.info(`Service is active on http://localhost:${port}`);
+  });
 }
 bootstrap();
