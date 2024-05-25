dc0a9e7f73c75856f63b815506d22cf40aa9dacc
Tommy <137893711+Tommy0901@users.noreply.github.com>
2024-05-25 13:38:34 +0000

    Sat, May 25, 2024, 9:38 PM +08:00

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,32 +1,32 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-export async function fetchCats(limit, order, page, breedIds = []) {
-  const params = {
-    api_key: process.env.API_KEY,
-    has_breeds: 1,
-    limit,
-    order,
-    page,
-  };
+// export async function fetchCats(limit, order, page, breedIds = []) {
+//   const params = {
+//     api_key: process.env.API_KEY,
+//     has_breeds: 1,
+//     limit,
+//     order,
+//     page,
+//   };
 
-  if (breedIds.length > 0) {
-    params['breed_ids'] = breedIds.join(',');
-  }
+//   if (breedIds.length > 0) {
+//     params['breed_ids'] = breedIds.join(',');
+//   }
 
-  try {
-    return (
-      await axios.get(`${process.env.API_URL}/V1/images/search`, { params })
-    ).data;
-  } catch (err) {
-    console.error("What's going on...", err);
-  }
-}
+//   try {
+//     return (
+//       await axios.get(`${process.env.API_URL}/V1/images/search`, { params })
+//     ).data;
+//   } catch (err) {
+//     console.error("What's going on...", err);
+//   }
+// }
 
-export async function fetchBreeds() {
-  try {
-    return (await axios.ge
-      t(`${process.env.API_URL}/V1/breeds`)).data;
-  } catch (err) {
-    console.error('Something bad happend...', err);
-  }
-}
+// export async function fetchBreeds() {
+//   try {
+//     return (await axios.ge
+//       t(`${process.env.API_URL}/V1/breeds`)).data;
+//   } catch (err) {
+//     console.error('Something bad happend...', err);
+//   }
+// }
